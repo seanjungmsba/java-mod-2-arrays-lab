ccc0a561042623fe4c2a5c0c5eb1cb27d32655a5
seanjungmsba <seanjeong1996@gmail.com>
2022-06-27 11:30:37 -0400

    Update

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,49 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        // your code here
+        
+        // initialize the array with length of 10
+        String[] studentNames = new String[10];
+
+        // intialize Scanner object
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter student's name: ");
+        studentNames[0] = sc.nextLine();
+
+        System.out.println("Enter student's name: ");
+        studentNames[1] = sc.nextLine();
+        
+        System.out.println("Enter student's name: ");
+        studentNames[2] = sc.nextLine();
+
+        System.out.println("Enter student's name: ");
+        studentNames[3] = sc.nextLine();
+
+        System.out.println("Enter student's name: ");
+        studentNames[4] = sc.nextLine();
+
+        System.out.println("Enter student's name: ");
+        studentNames[5] = sc.nextLine();
+
+        System.out.println("Enter student's name: ");
+        studentNames[6] = sc.nextLine();
+
+        System.out.println("Enter student's name: ");
+        studentNames[7] = sc.nextLine();
+
+        System.out.println("Enter student's name: ");
+        studentNames[8] = sc.nextLine();
+
+        System.out.println("Enter student's name: ");
+        studentNames[9] = sc.nextLine();
+
+        // print out the student names in the array
+        System.out.println("You have entered the following names: ");
+        for (String studentName: studentNames)
+            System.out.println(studentName);
+
     }
+
 }
